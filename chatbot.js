27fec95748ec4de746afb9d61db38fb07b769f74
luc234 <97034287+luciano234@users.noreply.github.com>
2025-02-26 16:56:25 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,5 @@
 (function() {
-    const WEBHOOK_URL = "https://hook.us1.make.com/7obx2jgtkfdp6i36mx78yamsguub13ah"; 
+    const WEBHOOK_URL = "https://still-dust-eca8.luciano351.workers.dev"; 
     const CHATBOT_NAME = "Blickbot Assistant";  
     const AVATAR_URL = "https://luciano234.github.io/jshosting/logo_v3_blickbot.jpg";  
  
