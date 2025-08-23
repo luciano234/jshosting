710c56710990e81d7204333b3fd1300510c3d6f0
luciano234 <luciano351@protinmail.com>
2025-08-22 20:19:40 -0400

    updated 20250822

---
+++ test_chatbot_blickbot_v2.js
@@ -0,0 +1,26 @@
+<!-- Widget Configuration -->
+    <!-- Widget Configuration -->
+    <script>
+        window.ChatWidgetConfig = {
+            webhook: {
+                url: 'https://n8n.srv960709.hstgr.cloud/webhook/60ffb14c-33df-40aa-a947-fa97caa12b69/chat',
+                route: 'general'
+            },
+            branding: {
+                logo: '<your company logo URL>',
+                name: 'nocodecreative.io', // Your company name
+                welcomeText: 'Hi 👋, how can we help?', //Welcome message
+                responseTimeText: 'We typically respond right away' //Response time message
+            },
+            style: {
+                primaryColor: '#854fff', //Primary color
+                secondaryColor: '#6b3fd4', //Secondary color
+                position: 'right', //Position of the widget (left or right)
+                backgroundColor: '#ffffff', //Background color of the chat widget
+                fontColor: '#333333' //Text color for messages and interface
+            }
+        };
+    </script>
+    <script src="https://cdn.jsdelivr.net/gh/WayneSimpson/n8n-chatbot-template@ba944c3/chat-widget.js"></script>
+    <!-- Widget Script -->
+    
